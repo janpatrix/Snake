57bdce29e251ce3ec7e59c888e0e4f59e5292a97
janpatrix <janpatrix@gmail.com>
2016-02-29 16:29:26 +0100

    Game Screen Finished

--- app/src/main/java/com/example/patrickgross/snake/MainActivity.java
+++ app/src/main/java/com/example/patrickgross/snake/MainActivity.java
@@ -6,12 +6,15 @@ import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Point;
 import android.graphics.Rect;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Display;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -23,10 +26,10 @@ public class MainActivity extends Activity {
     Rect rectToBeDrawn;
 
     //Frames
-    int frameHeight = 32;
-    int frameWidth = 32;
-    int numFrames = 6;
-    int frameNumber;
+    int frameHeight = 64;
+    int frameWidth = 64;
+    int frameNumberWidth = 0;
+    int frameNumberHeight = 0;
 
     int screenHeight;
     int screenWidth;
@@ -53,6 +56,37 @@ public class MainActivity extends Activity {
         i = new Intent(this, GameActivity.class);
     }
 
+    @Override
+    protected void onStop(){
+        super.onStop();
+        while(true){
+            snakeAnimView.pause();
+            break;
+        }
+        finish();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        snakeAnimView.resume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        snakeAnimView.pause();
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event){
+        if (keyCode == KeyEvent.KEYCODE_BACK) {
+            snakeAnimView.pause();
+            finish();
+            return true;
+        }
+        return false;
+    }
+
     class SnakeAnimView extends SurfaceView implements Runnable {
 
         Thread ourThread = null;
@@ -64,8 +98,8 @@ public class MainActivity extends Activity {
             super(context);
             ourHolder = getHolder();
             paint = new Paint();
-            frameWidth = headAnimBitmap.getWidth()/numFrames;
-            frameHeight = headAnimBitmap.getHeight();
+            frameWidth = headAnimBitmap.getWidth()/5;
+            frameHeight = headAnimBitmap.getHeight()/4;
         }
 
         @Override
@@ -73,8 +107,70 @@ public class MainActivity extends Activity {
             while (playingSnake){
                 update();
                 draw();
-                controlFPS():
+                controlFPS();
             }
+        }
+
+        public void update(){
+            rectToBeDrawn = new Rect(frameWidth*(3+frameNumberWidth) , frameHeight*frameNumberHeight, frameWidth*(4+frameNumberWidth), frameHeight*(frameNumberHeight+1));
+            frameNumberWidth++;
+            if(frameNumberWidth == 2){
+                frameNumberHeight += 1;
+                frameNumberWidth = 0;
+            }
+            if(frameNumberHeight == 2){
+                frameNumberHeight = 0;
+            }
+        }
+
+        public void draw(){
+            if(ourHolder.getSurface().isValid()){
+                canvas = ourHolder.lockCanvas();
+                canvas.drawColor(Color.BLACK);
+                paint.setColor(Color.argb(255, 255, 255, 255));
+                paint.setTextSize(150);
+                canvas.drawText("Snake", 10, 150, paint);
+                paint.setTextSize(50);
+                canvas.drawText("    Hi Score: " + hi, 10, screenHeight - 150, paint);
+
+                Rect destRect = new Rect(screenWidth/2 - 100, screenHeight/2 - 100,
+                        screenWidth/2 + 100, screenHeight/2 + 100);
+                canvas.drawBitmap(headAnimBitmap, rectToBeDrawn, destRect, paint);
+                ourHolder.unlockCanvasAndPost(canvas);
+            }
+        }
+
+        public void controlFPS(){
+            long timeThisFrame = (System.currentTimeMillis() - lastFrameTime);
+            long timeToSleep = 500 - timeThisFrame;
+            if (timeThisFrame > 0) {
+                fps = (int)(1000/timeThisFrame);
+            }
+            if (timeToSleep > 0){
+                try {
+                    ourThread.sleep(timeToSleep);
+                } catch (InterruptedException e) {}
+            }
+            lastFrameTime = System.currentTimeMillis();
+        }
+
+        public void pause() {
+            playingSnake = false;
+            try {
+                ourThread.join();
+            } catch (InterruptedException e){}
+        }
+
+        public void resume() {
+            playingSnake = true;
+            ourThread = new Thread(this);
+            ourThread.start();
+        }
+
+        @Override
+        public boolean onTouchEvent(MotionEvent motionEvent) {
+            startActivity(i);
+            return true;
         }
 
     }
